990d125d68754568d6d37bfbc79843c98c2e2cc4
Junqi Yang <542739645@qq.com>
2025-01-19 14:14:25 +0800

    add gitignore

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q+#)51fok8x3z0!07t&%oc)vnjhd@mckb-h_x9yl^kd+^m2&63
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["118.31.4.235"]
 
 
 # Application definition
