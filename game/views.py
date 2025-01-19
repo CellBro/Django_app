267abdb947f8aa9e4281e751d80ffbb71c5081d5
Junqi Yang <542739645@qq.com>
2025-01-19 15:47:03 +0800

    My first page

--- game/views.py
+++ game/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 def index(request):
-    
-    return HttpResponse("Hello_World!")
-
-# Create your views here.
+    line1='<h1 style="text-align:center">啵唧啵唧~</h1>'
+    line2='<img src="https://p0.ifengimg.com/pmop/2018/0212/90CBCCD8AFF18463F4B9297C90ABAB20DEF5392D_size16_w426_h434.jpeg" width=2000 >'
+    return HttpResponse(line1+line2)
