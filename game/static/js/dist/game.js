7a47b846fff8fce3d8538362467aa235a69837a1
Junqi Yang <542739645@qq.com>
2025-02-07 20:49:54 +0800

    Draw FireBall

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -178,10 +178,38 @@ class Player extends AcGameObject {
         this.playground.game_map.$canvas.mousedown(function(e){
             if(e.which === 3 ){
                 outer.move_to(e.clientX,e.clientY);
+            }else if(e.which === 1){
+                if(outer.cur_skill === "fireball")
+                {
+                    outer.shoot_fireball(e.clientX,e.clientY);
 
+                }
+          }
+            outer.cur_skill = null;
+
+        });
+
+        $(window).keydown(function(e){
+            if(e.which === 81 ) //q
+            {
+                outer.cur_skill = "fireball";
+                return false;
             }
+        });
+    }
 
-        })
+    shoot_fireball(tx,ty){
+        let x = this.x , y = this.y ;
+        let radius = this.playground.height * 0.01;
+        let angle = Math.atan2(ty-this.y,tx-this.x);
+        let vx = Math.cos(angle),vy = Math.sin(angle);
+        let color = "orange";
+        let speed = this.playground.height * 0.5;
+        let move_length = this.playground.height * 1.5;
+        new FireBall(this.playground,this,x,y,radius,vx,vy,color,speed,move_length);
+        
+
+
     }
 
     get_dist(x1,y1,x2,y2){
@@ -221,6 +249,53 @@ class Player extends AcGameObject {
         this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
+
+    }
+
+
+
+
+}
+class FireBall extends AcGameObject{
+    constructor(playground , player,x,y,radius,vx,vy,color,speed,move_length){
+        super();
+        this.playground = playground;
+        this.player = player;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.move_length =move_length;
+        this.eps = 0.1;
+
+
+    }
+
+    start(){}
+
+    update(){
+        if(this.move_length < this.eps){
+            this.destroy();
+            return false;
+        }
+        let moved = Math.min(this.move_length,this.speed * this.timedelta/1000);
+        this.x += this.vx*moved;
+        this.y +=this.vy*moved;
+        this.move_length -=moved;
+        this.render();
+
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+
 
     }
 
